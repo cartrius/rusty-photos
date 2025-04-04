d83710a513668da382f4f6d2488dd816096bf729
Cartrius Phipps <cartriusphipps@gmail.com>
2025-04-04 15:10:33 -0500

    Add image downloading and style changes

--- gallery-site/gallery.js
+++ gallery-site/gallery.js
@@ -1,15 +1,20 @@
 const imageUrls = [
-    "https://my-photo-bucket-cp.s3.amazonaws.com/processed/thumb_photo1.jpg",
-    "https://my-photo-bucket-cp.s3.amazonaws.com/processed/thumb_photo2.jpg",
+    "https://my-photo-bucket-cp.s3.amazonaws.com/processed/medium_matcha.jpg",
+    "https://my-photo-bucket-cp.s3.amazonaws.com/processed/thumb_matcha.jpg",
     "https://my-photo-bucket-cp.s3.amazonaws.com/processed/thumb_photo3.jpg"
   ];
   
   const gallery = document.getElementById("gallery");
   
   imageUrls.forEach(url => {
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = ""; // this triggers the browser's download behavior
+  
     const img = document.createElement("img");
     img.src = url;
     img.alt = "Processed image";
-    gallery.appendChild(img);
-  });
   
+    a.appendChild(img);
+    gallery.appendChild(a);
+  });
